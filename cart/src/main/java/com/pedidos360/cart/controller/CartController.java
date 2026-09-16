62b2fcc73dbc6c836a2c019aa4e9dfc1a82c4d5d
antonio <anto.mirandaa@duocuc.cl>
2026-09-16 20:34:03 -0300

    Update CrossOrigin annotation in CartController
    Removed localhost from CrossOrigin origins in CartController.

--- cart/src/main/java/com/pedidos360/cart/controller/CartController.java
+++ cart/src/main/java/com/pedidos360/cart/controller/CartController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:4200","https://52.22.2.226")
+@CrossOrigin(origins = "https://52.22.2.226")
 @RestController
 @RequestMapping("/cart")
 public class CartController {

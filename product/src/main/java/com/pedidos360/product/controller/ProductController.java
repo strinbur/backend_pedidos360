bb7065feb7f19ddf20f12b89740ebc45b83934fb
antonio <anto.mirandaa@duocuc.cl>
2026-09-16 20:34:37 -0300

    Update CrossOrigin annotation in ProductController

--- product/src/main/java/com/pedidos360/product/controller/ProductController.java
+++ product/src/main/java/com/pedidos360/product/controller/ProductController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:4200","https://52.22.2.226")
+@CrossOrigin(origins = "https://52.22.2.226")
 @RestController
 @RequestMapping("/products")
 public class ProductController {
